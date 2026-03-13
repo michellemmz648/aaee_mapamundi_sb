db46202c78d5ed26fe6388935cd51abd2a9074b7
Isidoro Nevares Martín <inevares@hotmail.com>
2026-03-13 13:46:33 +0100

    fichero configuración

--- src/main/java/org/lapaloma/mapamundi/vo/Continente.java
+++ src/main/java/org/lapaloma/mapamundi/vo/Continente.java
@@ -13,32 +13,35 @@ public class Continente {
     private String codigo;
     private String nombre;
 
-	public Continente(String codigo, String nombre) {
-		super();
-		this.codigo = codigo;
-		this.nombre = nombre;
-	}
+    public Continente(String codigo, String nombre) {
+        super();
+        this.codigo = codigo;
+        this.nombre = nombre;
+    }
 
-	public Continente() {
-		super();
-	}
-	
+    public Continente() {
+        super();
+    }
+
     public String getCodigo() {
-		return codigo;
-	}
-	public void setCodigo(String codigo) {
-		this.codigo = codigo;
-	}
-	public String getNombre() {
-		return nombre;
-	}
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
-	}
-	@Override
-	public String toString() {
-		return "Coche [codigo=" + codigo + ", nombre=" + nombre + "]";
-	}
+        return codigo;
+    }
 
-    
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    @Override
+    public String toString() {
+        return "Coche [codigo=" + codigo + ", nombre=" + nombre + "]";
+    }
+
 }
